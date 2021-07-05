69afff43302c6d6a5435a4d9f9b30d749fef1eb3
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-07-05 10:41:45 +0700

    throw error if no internet connection

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,49 +1,85 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import '../model/restaurant.dart';
 import '../model/restaurant_detail.dart';
+
+int timeout = 5;
 
 class ApiService {
   static final String _baseUrl = 'https://restaurant-api.dicoding.dev';
 
   Future<RestaurantsListResult> getRestaurants() async {
-    final response = await http.get(Uri.parse(_baseUrl + '/list'));
-    if (response.statusCode == 200) {
-      return RestaurantsListResult.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load restaurants list');
+    try {
+      final response = await http
+          .get(Uri.parse(_baseUrl + '/list'))
+          .timeout(Duration(seconds: timeout));
+      if (response.statusCode == 200) {
+        return RestaurantsListResult.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load restaurants list');
+      }
+    } on TimeoutException catch (e) {
+      throw Exception('Timeout error. Check your internet connection');
+    } on SocketException catch (e) {
+      throw Exception('Socket Error: $e');
+    } on Error catch (e) {
+      throw Exception('General Error: $e');
     }
   }
 
   Future<RestaurantsListResult> getRestaurantsResult(String query) async {
-    final response = await http.get(Uri.parse(_baseUrl + '/search?q=$query'));
-    if (response.statusCode == 200) {
-      return RestaurantsListResult.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load restaurants list');
+    try {
+      final response = await http
+          .get(Uri.parse(_baseUrl + '/search?q=$query'))
+          .timeout(Duration(seconds: timeout));
+      ;
+      if (response.statusCode == 200) {
+        return RestaurantsListResult.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load restaurants list');
+      }
+    } on TimeoutException catch (e) {
+      throw Exception('Timeout error. Check your internet connection');
+    } on SocketException catch (e) {
+      throw Exception('Socket Error: $e');
+    } on Error catch (e) {
+      throw Exception('General Error: $e');
     }
   }
 
   Future<RestaurantDetailResult> getRestaurantDetail(String id) async {
-    final response = await http.get(Uri.parse(_baseUrl + '/detail/$id'));
-    if (response.statusCode == 200) {
-      return RestaurantDetailResult.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load restaurants list');
+    try {
+      final response = await http
+          .get(Uri.parse(_baseUrl + '/detail/$id'))
+          .timeout(Duration(seconds: timeout));
+      ;
+      if (response.statusCode == 200) {
+        return RestaurantDetailResult.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load restaurants list');
+      }
+    } on TimeoutException catch (e) {
+      throw Exception('Timeout error. Check your internet connection');
+    } on SocketException catch (e) {
+      throw Exception('Socket Error: $e');
+    } on Error catch (e) {
+      throw Exception('General Error: $e');
     }
   }
 
-  Future<http.Response> addReview(String name, String title, String id) async {
-    return await http.post(
-        (Uri.parse('https://restaurant-api.dicoding.dev/review')),
-        headers: <String, String>{
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'X-Auth-Token': '12345'
-        },
-        body: {
-          "id": id,
-          "name": name,
-          "title": title,
-        });
-  }
+  // Future<http.Response> addReview(String name, String title, String id) async {
+  //   return await http.post(
+  //       (Uri.parse('https://restaurant-api.dicoding.dev/review')),
+  //       headers: <String, String>{
+  //         'Content-Type': 'application/x-www-form-urlencoded',
+  //         'X-Auth-Token': '12345'
+  //       },
+  //       body: {
+  //         "id": id,
+  //         "name": name,
+  //         "title": title,
+  //       });
+  // }
 }
